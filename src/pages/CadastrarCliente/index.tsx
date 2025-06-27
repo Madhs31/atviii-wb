10ecf1635dd6fc35a1aee30e113847be798a659d
Madhs31 <fernandinha.mf30@gmail.com>
2025-06-27 10:17:10 -0300

    ✨ feat Adicionando códigos

---
+++ src/pages/CadastrarCliente/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+const CadastroCliente: React.FC = () => {
+  return (
+    <div className="container-cadastro">
+      <div className="title-cadastro">
+        <h2>Cadastre um usuario</h2>
+      </div>
+      <div className="form-cadastro">
+        <form action="">
+          <p>Nome:</p>
+          <input type="text" placeholder="Digite o nome do cliente" />
+          <p>Nome Social:</p>
+          <input type="text" placeholder="Digite o nome social do cliente" />
+          <p>Genero:</p>
+          <input type="text" placeholder="Digite o genêro do cliente" />
+          <p>CPF:</p>
+          <input type="number" placeholder="Digite o CPF do cliente" />
+          <p>RG:</p>
+          <input type="number" placeholder="Digite o RG do cliente" />
+          <p>Telefone:</p>
+          <input type="text" placeholder="Digite o telefone do cliente" />
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CadastroCliente;
